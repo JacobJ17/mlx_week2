aa7dacd4a7b8975cd2ef156f3bcdfa2723a89d10
Jacob Jenner <jacobjenner19@gmail.com>
2025-06-17 12:54:56 +0100

    adding checkpoint loading logic.

--- train_cbow/01_train_cbow.py
+++ train_cbow/01_train_cbow.py
@@ -1,11 +1,13 @@
 # train_cbow.py
+import argparse
+import datetime
+import os
 
 import torch
 import tqdm
 import wandb
-import datetime
-from dataset import CBOW as CBOWDataset
 from cbow_model import CBOW as CBOWModel
+from dataset import CBOW as CBOWDataset
 from evaluate import topk
 
 
@@ -18,12 +20,37 @@ def get_dataloader(batch_size: int = 256, vocab_size=None, context_size=3) -> tu
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
     return dataset, dataloader
 
+def save_checkpoint(model, optimizer, run_name: str, epoch: int):
+    os.makedirs('checkpoints', exist_ok=True)
+    filename = f'checkpoints/{run_name}.epoch_{epoch}.pth'
+    torch.save({
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'epoch': epoch,
+    }, filename)
 
-def train(model, dataloader, optimizer, criterion, device, run_name, num_epochs=5):
+    artifact = wandb.Artifact('model-weights', type='model')
+    artifact.add_file(filename)
+    wandb.log_artifact(artifact)
+
+def load_checkpoint(model, optimizer, run_name: str):
+    checkpoints = [f for f in os.listdir('checkpoints') if f.startswith(run_name) and f.endswith('.pth')]
+    if not checkpoints:
+        return 0  # No checkpoint found, start from epoch 0
+    # Find the latest checkpoint by epoch number
+    latest = max(checkpoints, key=lambda x: int(x.split('_')[-1].split('.')[0].replace('epoch', '')))
+    path = os.path.join('checkpoints', latest)
+    checkpoint = torch.load(path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    print(f"Resumed from checkpoint: {path}")
+    return checkpoint['epoch']
+
+def train(model, dataloader, optimizer, criterion, device, run_name, num_epochs=5, start_epoch=1):
     model.to(device)
-    wandb.init(project='mlx-cbow', name=run_name)
+    wandb.init(project='mlx-cbow', name=run_name, resume="allow")
 
-    for epoch in range(1, num_epochs + 1):
+    for epoch in range(start_epoch, num_epochs + 1):
         progress = tqdm.tqdm(dataloader, desc=f"Epoch {epoch}", leave=False)
         for step, (inputs, targets) in enumerate(progress):
             inputs, targets = inputs.to(device), targets.to(device)
@@ -39,24 +66,21 @@ def train(model, dataloader, optimizer, criterion, device, run_name, num_epochs=
             if step % 10_000 == 0:
                 topk(model)
 
-        save_checkpoint(model, run_name, epoch)
+        save_checkpoint(model, optimizer, run_name, epoch)
 
     wandb.finish()
 
-
-def save_checkpoint(model, run_name: str, epoch: int):
-    filename = f'checkpoints/{run_name}.epoch_{epoch}.pth'
-    torch.save(model.state_dict(), filename)
-
-    artifact = wandb.Artifact('model-weights', type='model')
-    artifact.add_file(filename)
-    wandb.log_artifact(artifact)
-
-
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run_name', type=str, default=None, help='Name for this training run (used for checkpoints and wandb)')
+    args = parser.parse_args()
+
     torch.manual_seed(42)
-    timestamp = datetime.datetime.now().strftime('%Y_%m_%d__%H_%M_%S')
-    run_name = f'cbow_{timestamp}'
+    if args.run_name:
+        run_name = args.run_name
+    else:
+        timestamp = datetime.datetime.now().strftime('%Y_%m_%d__%H_%M_%S')
+        run_name = f'cbow_{timestamp}'
     device = get_device()
 
     vocab_size = 30000
@@ -65,16 +89,22 @@ def main():
     num_epochs = 10
     dataset, dataloader = get_dataloader(batch_size=batch_size, vocab_size=vocab_size, context_size=context_size)
 
-    print(f"Vocab Size: {vocab_size} out of a possible {len(dataset.int_to_vocab)}")
-
     model = CBOWModel(vocab_size=vocab_size, emb_dim=128)
     print('Model parameters:', sum(p.numel() for p in model.parameters()))
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
     criterion = torch.nn.CrossEntropyLoss()
 
-    train(model, dataloader, optimizer, criterion, device, run_name, num_epochs)
+    # Resume logic
+    os.makedirs('checkpoints', exist_ok=True)
+    start_epoch = 1
+    # Try to find a checkpoint for this run_name
+    checkpoints = [f for f in os.listdir('checkpoints') if f.startswith(run_name) and f.endswith('.pth')]
+    if checkpoints:
+        # Resume from latest checkpoint
+        start_epoch = load_checkpoint(model, optimizer, run_name) + 1
 
+    train(model, dataloader, optimizer, criterion, device, run_name, num_epochs)
 
 if __name__ == '__main__':
     main()
